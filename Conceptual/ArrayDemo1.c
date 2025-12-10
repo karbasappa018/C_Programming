d5308b2fdacea152b129e4a6b8df1b93bd9408d5
karbasappa018 <karbasappatadakale@gmail.com>
2025-12-11 00:10:57 +0530

    Array in C Programming

---
+++ Conceptual/ArrayDemo1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int Arr[4] = {10,20,30,40};
+    int Brr[] = {10,30,45 ,56};
+    int Crr[6]= {10,20,30};
+
+    printf("Size of Arr:%d\n",sizeof(Arr));
+    printf("Size of Brr:%d\n",sizeof(Brr));
+    printf("Size of Crr:%d\n",sizeof(Crr));
+
+
+
+
+    return 0;
+}
