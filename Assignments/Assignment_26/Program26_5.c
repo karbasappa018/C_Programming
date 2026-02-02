76fcbc7d2b5574630b298f1c3928b0d6ef37ca33
karbasappa018 <karbasappatadakale@gmail.com>
2026-02-02 22:08:32 +0530

    Assignment_26 Q5

---
+++ Assignments/Assignment_26/Program26_5.c
@@ -0,0 +1,34 @@
+/*
+   Accept number from user and display below pattern.
+    Input : 8
+    Output : 2   4   6   8   10  12  14  16
+*/
+#include<stdio.h>
+
+void Pattern(int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = 2; iCnt<=iNo*2; iCnt+=2)
+    {
+        printf("%d\t",iCnt);
+        
+    }
+
+    printf("\n");
+    
+}
+
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the Number of elements\n");
+    scanf("%d",&iValue);
+
+    Pattern(iValue);
+
+    return 0;
+
+}
