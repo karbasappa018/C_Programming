712ae40f90ab82d19a19dd2ca92e292f6708235c
karbasappa018 <karbasappatadakale@gmail.com>
2026-02-04 23:45:10 +0530

    Assignment_35 Q5

---
+++ Assignments/Assignment_35/program35_5.c
@@ -0,0 +1,18 @@
+/*   Write a program which accept string from user and display it inn 
+    reverse order. 
+    Input : “MarvellouS” 
+    Output : “SuollevraM”
+*/
+void Reverse(char *str) 
+{ 
+// Logic 
+} 
+int main() 
+{ 
+char arr[20]; 
+int iRet = 0; 
+printf("Enter string"); 
+scanf("%[^'\n']s",arr); 
+Reverse(arr); 
+return 0; 
+}
