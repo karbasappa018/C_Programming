9c44f8508756713e8b306cd2245192ab429a771e
karbasappa018 <karbasappatadakale@gmail.com>
2026-02-04 23:44:28 +0530

    Assignment_35 Q3

---
+++ Assignments/Assignment_35/program35_3.c
@@ -0,0 +1,27 @@
+/*   Write a program which accept string from user and return 
+    difference between frequency of small characters and frequency of 
+    capital characters. 
+    Input : “MarvellouS” 
+    Output : 6 (8-2) 
+
+*/
+int Difference(char *str) 
+{ 
+int iCnt = 0; 
+// Fileter 
+while(*src != '\0') 
+{ 
+// Logic 
+} 
+return ______; 
+}
+int main() 
+{ 
+char arr[20]; 
+int iRet = 0; 
+printf("Enter string"); 
+scanf("%[^'\n']s",arr); 
+iRet = Difference(arr); 
+printf("%d",iRet); 
+return 0; 
+}
