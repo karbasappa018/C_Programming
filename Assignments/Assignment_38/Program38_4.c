601beec9a7b075bf49bc4cace1aae0a7ac2fcedf
karbasappa018 <karbasappatadakale@gmail.com>
2026-02-28 22:38:48 +0530

    Assignment_38 Q4

---
+++ Assignments/Assignment_38/Program38_4.c
@@ -0,0 +1,50 @@
+/*
+
+    Write a program which accept string from user and copy that
+    characters of that string into another string by converting all capital
+    characters into small case.
+
+    Input : “Marvellous Python 2”
+
+    Output : “marvellous python 2”
+
+*/
+
+#include<stdio.h>
+
+void StrCpyCap(char *src, char *dest)
+{
+    char *temp = src;
+
+    while(*src != '\0')
+    {
+        if(*src >= 'A' && *src <='Z')
+        {
+            *src = *src+32;
+        }
+
+        src++;
+    }
+
+    src = temp;
+
+    while(*src != '\0')
+    {
+        *dest = *src;
+        dest++;
+        src++;
+    }
+
+    *dest = '\0';
+}
+
+int main()
+{
+    char arr[30]= "Marvellous Python 2";
+    char brr[30];
+
+    StrCpyCap(arr,brr);
+
+    printf("%s",brr);
+    return 0;
+}
